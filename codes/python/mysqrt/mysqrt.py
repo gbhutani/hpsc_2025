7f442d29e8ae5a99f558a7eb39a1548fb26a7b3b
Gaurav Bhutani <gaurav@iitmandi.ac.in>
2025-01-29 11:52:13 +0530

    included a check for convevrgence

--- codes/python/mysqrt/mysqrt.py
+++ codes/python/mysqrt/mysqrt.py
@@ -1,6 +1,12 @@
-x=2.0
+x=1000000.0
 s=1.0
-for k in range(10):
+kmax=1000
+tol = 1.0e-14
+for k in range(kmax):
+    s0=s
     s = 0.5*(s + x/s)
     print(f"At iteration {k+1}, the value s={s}")
+    delta_s = s-s0
+    if(abs(delta_s)<tol):
+        break
 print(f"The final value is s={s}")    
