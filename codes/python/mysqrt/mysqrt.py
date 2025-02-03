a99b1ac5969871acd5c9d7b82abc1daf479389be
Gaurav Bhutani <gaurav@iitmandi.ac.in>
2025-02-03 11:42:29 +0530

    converted to a function and added debug

--- codes/python/mysqrt/mysqrt.py
+++ codes/python/mysqrt/mysqrt.py
@@ -1,12 +1,15 @@
-x=1000000.0
-s=1.0
-kmax=1000
-tol = 1.0e-14
-for k in range(kmax):
-    s0=s
-    s = 0.5*(s + x/s)
-    print(f"At iteration {k+1}, the value s={s}")
-    delta_s = s-s0
-    if(abs(delta_s)<tol):
-        break
-print(f"The final value is s={s}")    
+def sqrt2(x, debug=False):
+    s=1.0
+    kmax=1000
+    tol = 1.0e-14
+    for k in range(kmax):
+        s0=s
+        s = 0.5*(s + x/s)
+        if debug:
+            print(f"At iteration {k+1}, the value s={s}")
+        delta_s = s-s0
+        if(abs(delta_s/x)<tol):
+            break
+    if debug:
+        print(f"The final value is s={s}")
+    return s    
