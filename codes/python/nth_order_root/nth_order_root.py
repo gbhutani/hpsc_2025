a9aefdda2ebf4a8f4df773f118408a2b0fb89475
Siddhartha-Acad <siddharthamithiya.acad2001@gmail.com>
2025-03-08 21:15:07 +0530

    First generalisation.
    Included functions f(x) and f'(x). It calculates the root by the general expression.
    
    x0 = xi - f(x)/f'(x)
    
    where, x0 iteratively approaches to the value of the root.
    and, xi is the initial guess value which is initiated to x0, to be used in the next loop.

--- codes/python/nth_order_root/nth_order_root.py
+++ codes/python/nth_order_root/nth_order_root.py
@@ -1,14 +1,21 @@
-import numpy as np
-
 
 n = 2			# order of root
 xi = 10			# initial guess
-a = 3			# root of the number a
+a = 25			# root of the number a :--> a**(1/n)
+
+
+def f(x):
+	return x**n - a
+
+def fp(x):
+	return n * x**(n-1)
 
 for i in range(10):
-	x0 = 0.5 * (xi + a/xi)
+	x0 = xi - f(xi) / fp(xi)
 
 	print(f'iteration {i}: x0 = {x0:.15f}')
 	xi = x0
 
-print(f'\nnumpy: {np.sqrt(a):.15f}, my: {x0:.15f}')
+
+
+print(f'\ncorrect value: {a ** (1/n):.15f}, my_estimation : {x0:.15f}')
