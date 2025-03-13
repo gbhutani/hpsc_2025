253ba996713c537f425c929233a55cba6d630a3f
Siddhartha-Acad <siddharthamithiya.acad2001@gmail.com>
2025-03-13 20:09:48 +0530

    modified the variables according to the lecture slides

--- codes/python/nth_order_root/nth_order_root.py
+++ codes/python/nth_order_root/nth_order_root.py
@@ -1,28 +1,46 @@
 
-def nth_order_root(n, a, xi=10, tolerance=1.0e-14, debug=False):
+def nth_order_root(n, x, init_guess=10, tolerance=1.0e-14, debug=False):
 	"""
-	Calculates the nth order root of a.
+	Computes the nth order root of a given number 'x', using Newton's method.
 	
-	n (real)    : order of root.
-	a (real)    : root of which number.
-	xi (real)   : initial guess value. Optional, default 10.
-	debug (bool): allows printing iterative results. Optional, default False.
+	Parameters:
+	-----------
+	n : float
+	    The order of the root (e.g., 2 for square root, 3 for cube root).
+	x : float
+	    The number whose nth root is to be calculated.
+	init_guess : float, optional (default=10)
+	    Initial guess for Newton's iteration.
+	tolerance : float, optional (default=1.0e-14)
+	    Convergence criterion for relative error.
+	debug : bool, optional (default=False)
+	    If True, prints iteration details.
 
-	returns		: nth order root of a.
+	Returns:
+	--------
+	float
+	    The computed nth root of 'x'.
+
+	Notes:
+	------
+	- Uses Newton-Raphson iteration: "s_{i+1} = s_i - f(s_i) / f'(s_i)".
+	- Stops iterating if the relative error is below the specified 'tolerance'.
+	- If 'debug=True', prints the progress of each iteration.
 	"""
+	
+	f = lambda s: s**n - x
+	fp = lambda s: n * s**(n-1)
 
-	f = lambda x: x**n - a
-	fp = lambda x: n * x**(n-1)
-
+	s_i = init_guess
 	for i in range(10):
-		x0 = xi - f(xi) / fp(xi)
-
-		rel_err = abs((x0 - xi) / xi)
+		s_0 = s_i - f(s_i) / fp(s_i)
+    
+		rel_err = abs((s_0 - s_i) / s_i)
 		if rel_err <= tolerance: break
-		if debug: print(f'iteration {i}: x0 = {x0:.15f}, rel_err = {rel_err:.5e}')
+		if debug: print(f'iteration {i}: s_i = {s_0:.15f}, rel_err = {rel_err:.5e}')
 
-		xi = x0
-	if debug: print(f'\ncorrect value: {a ** (1/n):.15f}, my_estimation : {x0:.15f}')
-	return x0
+		s_i = s_0
+	if debug: print(f'correct value: {x ** (1/n):.15f}, my_estimation : {s_0:.15f}\n')
+	return s_0
 
-# print(nth_order_root(n=2, a=4, debug=True))
+nth_order_root(n=2, x=4, debug=True)
